61faafbda4062a0c318e6b465630e849a4187825
danielcleto <danielcleto0440@gmail.com>
2017-07-07 22:06:48 -0400

    add csvs with data

---
+++ RestaurantsRecomendation/Utils/DataGenerator.cs
@@ -0,0 +1,14 @@
+﻿using FileHelpers;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantsRecomendation.Utils
+{
+    public class DataGenerator
+    {
+       
+    }
+}
